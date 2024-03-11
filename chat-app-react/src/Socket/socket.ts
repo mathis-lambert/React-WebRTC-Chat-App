5cf0b5e36479a1b8c5fdec8e7cf59e01f24f090b
MATHIS LAMBERT <mathis.lambert27@gmail.com>
2024-03-11 22:18:10 +0100

    [+] update config

--- chat-app-react/src/Socket/socket.ts
+++ chat-app-react/src/Socket/socket.ts
@@ -1,2 +1,4 @@
 import {io, Socket} from "socket.io-client";
-export const socketConn: Socket = io("http://localhost:3001", {transports: ["websocket"]});
+
+const uri = "http://localhost:3001";
+export const socketConn: Socket = io(uri, {transports: ["websocket"]});
